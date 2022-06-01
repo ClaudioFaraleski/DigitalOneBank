adc2403ecb66dca8403f5e8d9aee01e06614ab06
Claudio <faraleski@hotmail.com>
2022-06-01 17:09:10 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import com.one.digitalbank.Pessoa
+
+fun main(){
+
+    val claudio = Pessoa()
+
+    print("\no seu Nome  :"+claudio.nome)
+    print("\nO seu Cpf  :"+claudio.cpf)
+}
---
+++ src/main/kotlin/com/one/digitalbank/Pessoa.kt
@@ -0,0 +1,8 @@
+package com.one.digitalbank
+
+class Pessoa {
+
+    var nome: String = "Claudio"
+    var cpf: String = "123.123.123.44"
+
+}
